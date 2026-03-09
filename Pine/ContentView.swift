ea8ebca0016141327c11b0d63fe615943a10e104
Fedor Batonogov <fekinos@me.com>
2026-03-09 15:54:44 +0300

    Initial Commit

---
+++ Pine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pine
+//
+//  Created by Федор Батоногов on 09.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pine/PineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PineApp.swift
+//  Pine
+//
+//  Created by Федор Батоногов on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct PineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
